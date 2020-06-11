57a93997b3060e95a87d5ef41c75088b078eaf4c
Alexandre KOVAC <contact@kovacou.com>
2020-06-11 04:41:37 +0200

    add post parsing callbacks and 400/500 parse

--- renderer.go
+++ renderer.go
@@ -6,6 +6,8 @@
 package templates
 
 import (
+	"net/http"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"github.com/kovacou/go-types"
@@ -14,6 +16,7 @@ import (
 // contextID is an unique ID for the current runtime.
 var contextID = primitive.NewObjectID().Hex()
 
+// RenderFunc
 type RenderFunc func(code int, name string, data interface{}) error
 
 // New creates a new Renderer with the given callback.
@@ -29,28 +32,41 @@ type Renderer interface {
 	Parse404() error
 	Parse500() error
 	Parse(code int, name string) error
+	AddPostParsing(...func(data types.TSafeMap) error)
 }
 
 type renderer struct {
 	engine     Engine
 	data       types.TSafeMap
 	renderFunc RenderFunc
+	pc         []func(types.TSafeMap) error
 }
 
 func (e *renderer) Set(key string, v interface{}) {
 	e.data.Set(key, v)
 }
 
+func (e *renderer) AddPostParsing(c ...func(data types.TSafeMap) error) {
+	e.pc = append(e.pc, c...)
+}
+
 func (e *renderer) Parse(code int, name string) error {
 	e.data.Set("context", contextID)
 	e.data.Set("activeTemplate", name)
+
+	for _, c := range e.pc {
+		if err := c(e.data); err != nil {
+			return err
+		}
+	}
+
 	return e.renderFunc(code, name, e.data.Map())
 }
 
 func (e *renderer) Parse404() (err error) {
-	return
+	return e.Parse(http.StatusNotFound, "errors/404")
 }
 
 func (e *renderer) Parse500() (err error) {
-	return
+	return e.Parse(http.StatusInternalServerError, "errors/500")
 }
